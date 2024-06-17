9199808eb6785fdb676f9d2debf28ef7facfaeb7
Xandeco <>
2024-06-17 03:30:59 -0300

    quente!

--- Extrusora FATEC TechLab/src/main.cpp
+++ Extrusora FATEC TechLab/src/main.cpp
@@ -1,35 +1,107 @@
 #include <Arduino.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcdFofo(12, 11, 5, 4, 3, 2);
+//pin definition
+#define plusButtonPin 2
+#define scaleButtonPin 3
+#define minusButtonPin 4
+#define tempSensorPin A5
+#define relePin 5
+#define buzzerPin 6
 
+int plusButtonState = 0;
+int scaleButtonState = 0;
+int minusButtonState = 0;
 
+LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 
-float temperatura = 50;
+float scale = 1;
+float currentTemp = 0;
+float targetTemp = 0;
 
 void setup() {
-    lcdFofo.begin(16, 2);
-    lcdFofo.setCursor(0,0);
-    lcdFofo.print("FATEC TechLab");
-    lcdFofo.setCursor(0,1);
-    lcdFofo.print("Extrusora PET");
-    delay(1000);
+    pinMode(tempSensorPin, INPUT);
+    pinMode(relePin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
+
+    pinMode(plusButtonPin, INPUT);
+    pinMode(scaleButtonPin, INPUT);
+    pinMode(minusButtonPin, INPUT);
+
+    lcd.begin(16, 2);
+    lcd.setCursor(0, 0);
+    lcd.print("Current: ");
+    Serial.begin(9600);
 }
 
 void loop() {
-    lcdFofo.clear();
-    lcdFofo.setCursor(0,0);
-    lcdFofo.print("TEMP: ");
-    lcdFofo.print(temperatura, 1);
-    lcdFofo.setCursor(0,1);
-    if(temperatura < 80)
-    {
-        lcdFofo.print("ESQUENTANDO...");
-        temperatura += 1.5;
+    //temperature reading
+    currentTemp = analogRead(tempSensorPin);
+
+    //button handling
+    if (digitalRead(scaleButtonPin) == HIGH && scaleButtonState == 0) {
+        scaleButtonState = 1;
+
+        if (scale == 1) {
+            scale = 10;
+        } 
+        else if (scale == 10) {
+            scale = 0.1;
+        } 
+        else if (scale == 0.1) {
+            scale = 1;
+        }
     }
-    else
-    {
-        lcdFofo.print("PRONTO!");
+    else if (digitalRead(scaleButtonPin) == LOW) {
+        scaleButtonState = 0;
     }
-    delay(1000);
+
+    if (digitalRead(plusButtonPin) == HIGH && plusButtonState == 0) {
+        plusButtonState = 1;
+        targetTemp += scale;
+    }
+    else if (digitalRead(plusButtonPin) == LOW) {
+        plusButtonState = 0;
+    }
+
+    if (digitalRead(minusButtonPin) == HIGH && minusButtonState == 0) {
+        minusButtonState = 1;
+        targetTemp -= scale;
+        if (targetTemp < 0) {
+            targetTemp = 0;
+        }
+    }
+    else if (digitalRead(minusButtonPin) == LOW) {
+        minusButtonState = 0;
+    }
+
+    //temperature control
+    if (currentTemp < targetTemp) {
+        digitalWrite(relePin, HIGH);
+    } 
+    else {
+        digitalWrite(relePin, LOW);
+
+        if (currentTemp - targetTemp > 5) {
+            tone(buzzerPin, 1000, 1000);
+        }    
+        else {
+        }
+    }
+
+    //screen handling
+    lcd.setCursor(0, 0);
+    lcd.print("Cur:");
+    lcd.print(currentTemp, 1);
+    lcd.print("C");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Tar:");
+    lcd.print(targetTemp, 1);
+    lcd.print("C");
+
+    lcd.setCursor(12, 1);
+    //pint the scale with 1 decimal
+    lcd.print("S");
+    lcd.print(scale, 1);
 }
